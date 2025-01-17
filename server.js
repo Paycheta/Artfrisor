23eedd5add5f2be26b24f4ddd951ee6e263e0a79
Pavle Jovanovic <paycheta@gmail.com>
2025-01-17 19:08:24 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Set the port
+const PORT = 3000;
+
+// Serve static files (CSS, images, etc.)
+app.use(express.static(path.join(__dirname, "public")));
+
+// Define the main route
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
